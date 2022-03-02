0948b50fa9d7032acf88675cd3c4c414b57ddbf6
shakhawath <m.shakhawath18@gmail.com>
2022-03-02 09:18:06 +0600

    small correction

--- js/app.js
+++ js/app.js
@@ -122,18 +122,18 @@ const showDetails = phone => {
                     <h6 class="card-title">${phone.brand}</h6>
                     <p class="card-text">${phone.slug}</p>
                     <p class="card-text"><span>Main Features</span></p>
-                    <p class="card-text"><span>Display:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'Not available'}</p>
-                    <p class="card-text"><span>Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'Not available'}</p>
-                    <p class="card-text"><span>Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'Not available'}</p>
-                    <p class="card-text"><span>Storage:</span> ${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'Not available'}</p>
-                    <p class="card-text"><span>Sensors:</span> ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : 'Not available'}</p>
+                    <p class="card-text"><span>Display:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : ''}</p>
+                    <p class="card-text"><span>Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : ''}</p>
+                    <p class="card-text"><span>Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : ''}</p>
+                    <p class="card-text"><span>Storage:</span> ${phone.mainFeatures.storage ? phone.mainFeatures.storage : ''}</p>
+                    <p class="card-text"><span>Sensors:</span> ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : ''}</p>
                     <p class="card-text"><span>Others Features</span></p>
-                    <p class="card-text"><span>WLAN:</span> ${phone?.others?.WLAN ? phone?.others?.WLAN : 'Not available'}</p>
-                    <p class="card-text"><span>Bluetooth:</span> ${phone?.others?.Bluetooth ? phone?.others?.Bluetooth : 'Not available'}</p>
+                    <p class="card-text"><span>WLAN:</span> ${phone?.others?.WLAN ? phone?.others?.WLAN : ''}</p>
+                    <p class="card-text"><span>Bluetooth:</span> ${phone?.others?.Bluetooth ? phone?.others?.Bluetooth : ''}</p>
                     <p class="card-text"><span>GPS:</span> ${phone?.others?.GPS ? phone?.others?.GPS : 'Not available'}</p>
-                    <p class="card-text"><span>NFC:</span> ${phone?.others?.NFC ? phone?.others?.NFC : 'Not available'}</p>
-                    <p class="card-text"><span>Radio:</span> ${phone?.others?.Radio ? phone?.others?.Radio : 'Not available'}</p>
-                    <p class="card-text"><span>USB:</span> ${phone?.others?.USB ? phone?.others?.USB : 'Not available'}</p>
+                    <p class="card-text"><span>NFC:</span> ${phone?.others?.NFC ? phone?.others?.NFC : ''}</p>
+                    <p class="card-text"><span>Radio:</span> ${phone?.others?.Radio ? phone?.others?.Radio : ''}</p>
+                    <p class="card-text"><span>USB:</span> ${phone?.others?.USB ? phone?.others?.USB : ''}</p>
                 </div>
             `;
     detailsContainer.appendChild(details);
